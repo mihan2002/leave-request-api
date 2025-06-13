74f6ec345e5b57a799daec9cc69c14b1b0dbd8c0
mihan2002 <mihanamajith20@gmail.com>
2025-06-13 07:26:00 +0530

    logging updated

--- src/main/java/com/mihan/leave_request_api/controller/LeaveRequestController.java
+++ src/main/java/com/mihan/leave_request_api/controller/LeaveRequestController.java
@@ -5,6 +5,7 @@ import com.mihan.leave_request_api.model.LeaveRequest;
 import com.mihan.leave_request_api.service.LeaveRequestService;
 import com.mihan.leave_request_api.service.AuditService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/leave-requests")
 public class LeaveRequestController {
